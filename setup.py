9471272537f13fe0dbb8e1bc9573ccae15b598a6
Emile Roux <emile.roux@univ-smb.fr>
2020-10-16 11:23:26 +0200

    first One

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+import templatePyMod
+
+setup(name='templatePyMod',
+      version=pycama.__version__,
+      description="templatePyMod",
+      long_description="templatePyMod",
+      author="Ludovic Charleux, Emile Roux",
+      author_email="emile.roux@univ-smb.fr",
+      license="GPL v3",
+      packages=["templatePyMod"],
+      zip_safe=False,
+      include_package_data=True,
+      url="https://gitlab.com/xxx",
+      install_requires=[
+          "numpy",
+          "scipy",
+          "pandas",
+      ],
+      )
---
+++ test/test_Dummy.py
@@ -0,0 +1,14 @@
+import numpy as np
+import os
+import templatePyMod as TPM
+
+
+
+res = TPM.dummy.func(5.)
+
+def test_points():
+    assert (res == np.sqrt(5.))
+
+
+if __name__ == '__main__':
+    print(test_points())
